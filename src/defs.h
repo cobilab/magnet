ae511b58cd763e399b05c08ca30422056cf76c8e
pratas <6882743+pratas@users.noreply.github.com>
2023-10-04 10:16:12 +0100

    info

--- src/defs.h
+++ src/defs.h
@@ -22,8 +22,8 @@ typedef int8_t   I8;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-#define RELEASE                4
-#define VERSION                19
+#define RELEASE                2
+#define VERSION                23
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
